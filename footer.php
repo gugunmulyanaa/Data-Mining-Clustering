050e564e36201e9ffeeadcef5fb8808114e31875
gugunmulyanaa <72129588+gugunmulyanaa@users.noreply.github.com>
2021-06-11 11:45:39 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 
 	<div class="container2" style="border:1px solid #d2d2d7; background-color:#d2d2d7;">
-		<center><h3><b></b></h3></center>
+		<center><h3><b>gugunmulyana</b></h3></center>
 	</div>
